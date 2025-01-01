9c5e5f0948c5d879aff2fafe9add9dba0a44efb2
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-02 04:29:56 +0530

    Persist speedLevel across sessions
    So that the user doesn't have to use the SPEED command to set speedLevel everytime they open up the game.

--- data/speedhack_indicator/init.lua
+++ data/speedhack_indicator/init.lua
@@ -1,8 +1,22 @@
 local speedHackImg = Hyperspace.Resources:GetImageId("statusUI/speedhack.png")
 local whiteColor = Graphics.GL_Color(1, 1, 1, 1)
 
+-- References-
+-- FTL Insurrection+ for Hyperspace- speedcontrol.lua, events.xml:351, events.xml:396
+
+script.on_init(function()
+	-- Metavars needs be accessed on init seemingly, will give wrong value otherwise
+	Hyperspace.FPS.speedLevel = Hyperspace.metaVariables.speedHackIndicatorSpeedLevel
+	Hyperspace.FPS.speedEnabled = false
+end)
+
 script.on_render_event(Defines.RenderEvents.FTL_BUTTON, function()
-	if Hyperspace.FPS.speedEnabled and Hyperspace.FPS.speedLevel ~= 0 then
-		Hyperspace.Resources:RenderImage(speedHackImg, 1208, 606, 0, whiteColor, 1.0, false)
+	local speedLevel = Hyperspace.FPS.speedLevel
+	if Hyperspace.FPS.speedEnabled and speedLevel ~= 0 then
+		Hyperspace.metaVariables.speedHackIndicatorSpeedLevel = speedLevel
+
+		Hyperspace.Resources:RenderImage(speedHackImg, 1206, 606, 0, whiteColor, 1.0, false)
 	end
 end, function() end)
+
+-- todo: tooltip
