573369c9ca2a77fef118a15a80f28452a134b17e
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-01 16:42:05 +0530

    Remove showIndicator redundant logic
    Since the check to show or not can be done in the FTL_BUTTON hook itself.

--- data/speedhack_indicator/init.lua
+++ data/speedhack_indicator/init.lua
@@ -1,14 +1,8 @@
 local speedHackImg = Hyperspace.Resources:GetImageId("statusUI/speedhack.png")
 local whiteColor = Graphics.GL_Color(1, 1, 1, 1)
 
-local showIndicator = false
-
 script.on_render_event(Defines.RenderEvents.FTL_BUTTON, function()
-	if showIndicator then
+	if Hyperspace.FPS.speedEnabled and Hyperspace.FPS.speedLevel ~= 0 then
 		Hyperspace.Resources:RenderImage(speedHackImg, 1208, 606, 0, whiteColor, 1.0, false)
 	end
 end, function() end)
-
-script.on_internal_event(Defines.InternalEvents.ON_TICK, function()
-	showIndicator = Hyperspace.FPS.speedEnabled and Hyperspace.FPS.speedLevel ~= 0
-end)
