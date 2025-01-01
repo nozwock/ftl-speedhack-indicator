060584cd5c69d5c5988ba9833000d5e9bef75e50
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-01 16:27:52 +0530

    Fix build script returning curropted zip
    Due to using .ftl extension, since git seems to use extension to determine
    the output type.

--- build.sh
+++ build.sh
@@ -4,4 +4,9 @@ shopt -s extglob
 CWD="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
 
 # zip -r "$(dirname "$CWD")/$(basename "$CWD").ftl" !(build.sh)
-git archive -o "$(dirname "$CWD")/$(basename "$CWD").ftl" main
+cd "$CWD"
+
+modFileName="$(basename "$CWD")"
+git archive -o "$modFileName.zip" main
+
+mv "$modFileName.zip" "../$modFileName.ftl"
