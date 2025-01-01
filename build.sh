441de54bc0fef1d7f8fe97dbbb68cd180c878286
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-01 16:41:12 +0530

    Improve build script to include unstaged changes

--- build.sh
+++ build.sh
@@ -3,10 +3,5 @@ shopt -s extglob
 
 CWD="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
 
-# zip -r "$(dirname "$CWD")/$(basename "$CWD").ftl" !(build.sh)
-cd "$CWD"
-
 modFileName="$(basename "$CWD")"
-git archive -o "$modFileName.zip" main
-
-mv "$modFileName.zip" "../$modFileName.ftl"
+zip -r "$(dirname "$CWD")/$modFileName.ftl" !(.git)
