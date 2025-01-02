3c46ff1456cb964d2b4bb7dc7428b21bd038cb63
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-02 10:55:38 +0530

    Add tooltip to indicator

--- data/speedhack_indicator/init.lua
+++ data/speedhack_indicator/init.lua
@@ -1,5 +1,68 @@
-local speedHackImg = Hyperspace.Resources:GetImageId("statusUI/speedhack.png")
-local whiteColor = Graphics.GL_Color(1, 1, 1, 1)
+---@class GL_Texture
+
+---@class Point
+---@field x integer
+---@field y integer
+
+---@class Image
+---@field data? ImageData
+local Image = {}
+
+do
+	local imageMetaTable = { __index = Image }
+	local defaultColor = Graphics.GL_Color(1, 1, 1, 1)
+
+	---@param texture GL_Texture
+	---@param pos Point
+	function Image.new(texture, pos)
+		local img = {
+			---@class ImageData
+			data = {
+				texture = texture,
+				position = pos,
+			},
+		}
+		setmetatable(img, imageMetaTable)
+		---@cast img Image
+
+		return img
+	end
+
+	function Image:render()
+		if not self.data then
+			log("Can't render an image without texture and position")
+			return
+		end
+		Hyperspace.Resources:RenderImage(
+			self.data.texture,
+			self.data.position.x,
+			self.data.position.y,
+			0,
+			defaultColor,
+			1,
+			false
+		)
+	end
+end
+
+---@param text string
+---@param x integer
+---@param y integer
+---@param width integer
+---@param height integer
+local function renderTooltip(text, x, y, width, height)
+	local mousePos = Hyperspace.Mouse.position
+	if not mousePos then
+		log("Failed to get mouse position")
+		return
+	end
+
+	if (mousePos.x >= x and mousePos.x <= x + width) and (mousePos.y >= y and mousePos.y <= y + height) then
+		Hyperspace.Mouse:SetTooltip(text)
+	end
+end
+
+local indicatorImage = Image.new(Hyperspace.Resources:GetImageId("statusUI/speedhack.png"), Hyperspace.Point(1218, 622))
 
 -- References-
 -- FTL Insurrection+ for Hyperspace- speedcontrol.lua, events.xml:351, events.xml:396
@@ -14,8 +77,14 @@ script.on_render_event(Defines.RenderEvents.FTL_BUTTON, function()
 	local speedLevel = Hyperspace.FPS.speedLevel
 	if Hyperspace.FPS.speedEnabled and speedLevel ~= 0 then
 		Hyperspace.metaVariables.speedHackIndicatorSpeedLevel = speedLevel
-		Hyperspace.Resources:RenderImage(speedHackImg, 1218, 622, 0, whiteColor, 1.0, false)
+
+		indicatorImage:render()
+		renderTooltip(
+			string.format("Speed Level: %dx", speedLevel),
+			indicatorImage.data.position.x,
+			indicatorImage.data.position.y,
+			32,
+			33
+		)
 	end
 end, function() end)
-
--- todo: tooltip
