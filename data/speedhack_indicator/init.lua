df8b5d5c9c7c2ff7db151b523501f227ae7792b6
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-02 06:42:34 +0530

    Make the image smaller

--- data/speedhack_indicator/init.lua
+++ data/speedhack_indicator/init.lua
@@ -14,8 +14,7 @@ script.on_render_event(Defines.RenderEvents.FTL_BUTTON, function()
 	local speedLevel = Hyperspace.FPS.speedLevel
 	if Hyperspace.FPS.speedEnabled and speedLevel ~= 0 then
 		Hyperspace.metaVariables.speedHackIndicatorSpeedLevel = speedLevel
-
-		Hyperspace.Resources:RenderImage(speedHackImg, 1206, 606, 0, whiteColor, 1.0, false)
+		Hyperspace.Resources:RenderImage(speedHackImg, 1218, 622, 0, whiteColor, 1.0, false)
 	end
 end, function() end)
 
