667a9d10b04179612f639645da22f040c0db2997
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-02 11:50:03 +0530

    Update build script to take symlinked project dir into consideration

--- build.sh
+++ build.sh
@@ -2,6 +2,15 @@
 shopt -s extglob
 
 CWD="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
+cd "$CWD"
 
 modFileName="$(basename "$CWD")"
-zip -r "$(dirname "$CWD")/$modFileName.ftl" !(.git)
+modsDir="$(realpath -s "$1")"
+
+if [ -d "$modsDir" ]; then
+    echo Saving as \`"$modsDir/$modFileName.ftl"\`
+    zip -r "$modsDir/$modFileName.ftl" !(.git)
+else
+    echo "Path to slipstream's mods/ directory is required!"
+    exit 1
+fi
